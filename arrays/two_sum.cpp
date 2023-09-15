a30f0ff4c32684dd870c715adcda331b9f9df0f6
sweta thakur <99349185+sweta-thakur112@users.noreply.github.com>
2023-09-15 12:24:01 +0530

    Create two_sum.cpp

---
+++ arrays/two_sum.cpp
@@ -0,0 +1,14 @@
+problem link - https://leetcode.com/problems/two-sum/description/
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        or (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums[i] + nums[j] == target) {
+                    return {i, j};
+                }
+            }
+        }
+        return {};
+    }
+};
