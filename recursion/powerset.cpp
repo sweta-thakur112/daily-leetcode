3f401c6337dfa14b851e8cc28e1e7ed39fdc63f6
sweta thakur <99349185+sweta-thakur112@users.noreply.github.com>
2023-10-14 13:34:06 +0530

    Update powerset.cpp

--- recursion/powerset.cpp
+++ recursion/powerset.cpp
@@ -1,1 +1,17 @@
 
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>>v;
+        for(int i=0;i<=(1<<nums.size())-1;i++)
+        {
+            vector<int>ans;
+            for(int j=0;j<nums.size();j++)
+            {
+                if(i & (1 << j)) ans.push_back(nums[j]);
+            }
+          v.push_back(ans);
+        }
+        return v;
+    }
+};
